16e1c19f041f876d200459db647b148dc08ab94b
Vida-CruX <esrkforward@gmail.com>
2026-05-23 22:56:22 +0800

    Initial Commit

---
+++ GhosttyBoo Screen Saver/GhosttyBoo_Screen_SaverView.h
@@ -0,0 +1,12 @@
+//
+//  GhosttyBoo_Screen_SaverView.h
+//  GhosttyBoo Screen Saver
+//
+//  Created by 陈鹤 on 23/5/2026.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface GhosttyBoo_Screen_SaverView : ScreenSaverView
+
+@end
---
+++ GhosttyBoo Screen Saver/GhosttyBoo_Screen_SaverView.m
@@ -0,0 +1,51 @@
+//
+//  GhosttyBoo_Screen_SaverView.m
+//  GhosttyBoo Screen Saver
+//
+//  Created by 陈鹤 on 23/5/2026.
+//
+
+#import "GhosttyBoo_Screen_SaverView.h"
+
+@implementation GhosttyBoo_Screen_SaverView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
